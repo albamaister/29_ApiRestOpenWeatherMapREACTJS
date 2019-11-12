34dc6f828261a2dabb1b88f67f9941d0b1dcaa0a
Andres Alba <bryanalba92@gmail.com>
2019-11-12 00:00:53 -0500

    Separación de servicios, constantes

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import convert  from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import transformWeather from './../../services/transformWeather';
 import './styles.css';
+import { api_weather } from './../../constants/api_url';
 import {
-    CLOUD,
-    CLOUDY,
     SUN,
-    WINDY,
-    RAIN,
-    SNOW,
+
 } from './../../constants/weathers';
-
-const location = "Buenos Aires,ar";
-const api_key = "9fc91d474fa5a26c776d69aacff23360";
-const url = "http://api.openweathermap.org/data/2.5/weather";
-
-const api_weather = `${url}?q=${location}&appid=${api_key}`;
 
 const data = {
     temperature: 5,  
@@ -31,34 +22,29 @@ class WeatherLocation extends Component {
     constructor() {
         super();
         this.state = {
-            city: 'Buenos Aires',
+            city: 'Cuencas',
             data: data
         };
+        // console.log("constructor");
     }
 
-    getTemp = (kelvin) => {
-        return convert(kelvin).from('K').to('C').toFixed(2);
-    }
+    // componentDidMount() {
+    //     console.log("componentDidMount");
+    // }
 
-    getWeatherState = (weather_data) => {
-        return SUN;
-    }
+    // componentDidUpdate(prevProps, prevState) {
+    //     console.log("componentDidUpdate");
+    // }
+    // componentWillMount() {
+    //     console.log("UNSAFE componentWillMount");
+    // }
+    
 
-    getData = (weather_data) => {
-        const { humidity, temp } = weather_data.main;
-        const { speed } = weather_data.wind;
-        const weatherState = this.getWeatherState(weather_data) ;
-        const temperature = this.getTemp(temp);
+    // componentWillUpdate(nextProps, nextState) {
+    //     console.log("UNSAFE componentWillUpdate");
+    // }
 
-        const data = {
-            humidity,
-            temperature,
-            weatherState,
-            wind: `${ speed } m/s`
-        };
 
-        return data;
-    };
 
     handleUpdateClick = () => {
         fetch(api_weather).then( resolve => {
@@ -66,7 +52,7 @@ class WeatherLocation extends Component {
             return resolve.json();
 
         }).then( data => {
-            const   newWeather = this.getData(data);
+            const newWeather = transformWeather(data);
             console.log(newWeather);
             this.setState({
                 data: newWeather
@@ -75,6 +61,7 @@ class WeatherLocation extends Component {
        
     }
     render () {
+        console.log("render");
         const { city, data } = this.state;
         return(
             <div className="weatherLocationCont">  
---
+++ src/constants/api_url.js
@@ -0,0 +1,4 @@
+const location = "Cuenca,ec";
+const api_key = "9fc91d474fa5a26c776d69aacff23360";
+const url = "http://api.openweathermap.org/data/2.5/weather";
+export const api_weather = `${url}?q=${location}&appid=${api_key}`;

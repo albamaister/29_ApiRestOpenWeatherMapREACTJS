3337ceeb12d6bcc72f0c6ffaac0bf209d32c5717
Andres Alba <bryanalba92@gmail.com>
2019-12-09 01:06:05 -0500

    Pronostico extendido y componente ForecastItem para pronostico extendido

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import "./App.css";
 import { Grid ,Col, Row } from 'react-flexbox-grid';
 import LocationList from "./components/LocationList";
+import ForecastExtended from "./components/ForecastExtended";
+import { MuiThemeProvider } from "@material-ui/core";
+
 
 
 const cities = [
@@ -18,37 +21,48 @@ const cities = [
 ];
 // function App() {
     class App extends Component {
+
+        constructor() {
+            super();
+            this.state = { city: null };
+        }
         handleSelectedLocation = city => {
+            this.setState({ city });
             console.log(`handleSelectedLocation ${city}`);
         }
 
         render() {
-            return ( 
-                <Grid>
-                    <Row>
-                        <AppBar position='sticky'>
-                            <Toolbar>
-                                <Typography variant='title' color='inherit'>
-                                    Weather App
-                                </Typography>
-                            </Toolbar>
-                        </AppBar>
-                    </Row>
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}></LocationList>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <Paper elevation={4}>
-                                
-                            <div className="details">
-
-                            </div>
-                            </Paper>
-                        </Col>     
-                    </Row>
-                </Grid> 
-        );
+            const { city } = this.state
+            return (
+                <MuiThemeProvider>
+                    <Grid>
+                        <Row>
+                            <Col xs={12}>
+                                <AppBar title="Weather App"></AppBar>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} md={6}>
+                                <LocationList
+                                    cities={cities} 
+                                    onSelectedLocation={this.handleSelectedLocation}>
+                                </LocationList>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Paper elevation={4}>
+                                    
+                                    <div className="details">
+                                        { 
+                                            city && 
+                                                <ForecastExtended city={city}></ForecastExtended>                                                
+                                        }
+                                    </div>
+                                </Paper>
+                            </Col>     
+                        </Row>
+                    </Grid>         
+                </MuiThemeProvider> 
+                );
 }
 }
 export default App;
